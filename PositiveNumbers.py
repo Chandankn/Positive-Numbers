f52dbc3f2ca14c02b4b8b702c9ba2140614d1333
Chandankn <87237018+Chandankn@users.noreply.github.com>
2021-07-14 06:29:51 -0700

    created by chandan
    developed by positive numbers .py

---
+++ PositiveNumbers.py
@@ -0,0 +1,20 @@
+#include<studio.h>
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,95,3]
+
+for num in list1:
+
+  if num>=0;
+     print(num,end = "")
+     
+output : 12,5,64
+
+#include<studio.h>
+list2 = [12,14,-93,3]
+
+for numin list2
+
+if num>=0
+   print(num,end = "")
+   
+output : 12,14,3
